0e5b0e2f7aa901e98770640241fcb0e64c04fca2
leonardogedler <leogedler@hotmail.com>
2018-03-22 22:43:26 -0400

    Created signup form

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -6,16 +6,34 @@ import * as actions from '../../actions';
 class Signup extends Component {
 
     renderField(field) {
+        const { meta: { touched, error } } = field;
+
+        console.log('error', error);
         return (
             <fieldset className="form-group">
                 <label>{field.label}:</label>
                 <input {...field.input} type={field.type} className="form-control" />
+                <div className="error">
+                    {touched ? error : ''}
+                </div>
             </fieldset>
         )
     }
 
-    handleFormSubmit(values){
-        console.log('values', values);
+    handleFormSubmit = (values) => {
+        console.log('Submit', values);
+        // Call action creator
+        this.props.signupUser(values);
+    }
+
+    renderAlert() {
+        if (this.props.errorMessage) {
+            return (
+                <div className="alert alert-danger">
+                    <strong>Oops!</strong> {this.props.errorMessage}
+                </div>
+            )
+        }
     }
 
     render() {
@@ -41,6 +59,7 @@ class Signup extends Component {
                     type="password"
                     component={this.renderField}
                 />
+                {this.renderAlert()}
                 <button action="submit" className="btn btn-primary">Sign up!</button>
             </form>
         )
@@ -48,6 +67,36 @@ class Signup extends Component {
 }
 
 
+function validate(values) {
+    const errors = {};
+
+    if (!values.email) {
+        errors.email = 'Please enter an email'
+    }
+
+    if (!values.passwordConfirm) {
+        errors.passwordConfirm = 'Please enter a password confirmation'
+    }
+
+    if (!values.password) {
+        errors.password = 'Please enter a password'
+    }
+
+    if (values.password !== values.passwordConfirm) {
+        errors.password = 'Passwords must match';
+    }
+
+    return errors;
+}
+
+
+function mapStateToProps(state) {
+    return { errorMessage: state.auth.error }
+}
+
 export default reduxForm({
-    form: 'signup'
-})(Signup);
+    form: 'signup',
+    validate
+})(
+    connect(mapStateToProps, actions)(Signup)
+);
